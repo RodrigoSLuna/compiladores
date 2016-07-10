97202e1f308167caffab15dd9a210bb5e5e58ae6
VictorGee <victortaichou@gmail.com>
2016-07-10 12:16:19 -0300

    Adicionando a entrada
    Adicionando a entrada que será testada. O programa deve gerar o  Hello
    World! do C, se tudo estiver certo

--- entrada.pas
+++ entrada.pas
@@ -1,6 +1,34 @@
-Program HelloWorld;
+// Program HelloWorld;
 
-Begin
-  WriteLn( 'hello, world' );
-  Writeln( 3 );
-END.
+// Begin
+//   WriteLn( 'hello, world' );
+//   Writeln( 3 );
+// END.
+
+//Esse codigo deve gerar o Hello World do C
+Algoritmo Hello;
+
+:)
+	Writeln('Hello World!');
+	WriteLn(3);
+:(.
+
+// Algoritmo Main;
+
+// PARA_NAO_ESCREVER_TUDO_DE_NOVO foo : SemCasaDecimal;
+// :)
+// 	a <- 4;
+// :(
+// ;
+
+// Legenda
+// 	K : CharPointerMelhorado;
+// 	N : SemCasaDecimal;
+
+// :)
+// 	BebaEnquanto(a > 2)
+// 		:)
+// 			a <- 6;
+// 		:(
+// 		;
+// :(.
